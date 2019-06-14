0eefb8902fa5cfed9b12734610a2e14848b56e21
Paulo Sousa <prsousa34@gmail.com>
2019-06-14 21:51:08 +0100

    [Feature] Basic setup
    - Changes page title
    - Adds custom logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src="/logo.png" className="App-logo" alt="logo" />
+        <p>Q-Música é?</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jogar
         </a>
       </header>
     </div>
